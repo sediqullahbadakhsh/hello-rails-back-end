f7ebd27ad40b815581fac0d05965cdbe000ce5fc
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-09 21:08:07 +0430

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'static/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static#index'
+  resources :greetings, only: [:index]
+  
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
