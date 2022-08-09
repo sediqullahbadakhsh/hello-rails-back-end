5177612f9bf7f5eb9b26d76e34a0c57ef7bbcacd
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-08-09 22:19:17 +0430

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  root 'static#index'
-  resources :greetings, only: [:index]
+
+
+   get 'greetings', to: 'greetings#index'
   
 
-  get '*page', to: 'static#index', constraints: ->(req) do
-    !req.xhr? && req.format.html?
-  end
 end
